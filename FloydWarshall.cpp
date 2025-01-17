50558b4a62ef844233cf826ca80d5b73e680e19d
Khushi Joshi <khushijoshi740@gmail.com>
2025-01-17 17:35:14 +0530

    Add Floyd Warshall

---
+++ FloydWarshall.cpp
@@ -0,0 +1,62 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    void shortestDistance(vector<vector<int>>& mat) {
+        int n=mat.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==-1){
+                    mat[i][j]=1e9;
+                }
+                if(i==j){
+                    mat[i][j]=0;
+                }
+            }
+        }
+        for(int k=0;k<n;k++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    if(mat[i][j]>mat[i][k]+mat[k][j]){
+                        mat[i][j]=mat[i][k]+mat[k][j];
+                    }
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==1e9){
+                    mat[i][j]=-1;
+                }
+            }
+        }
+    }
+};
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> matrix(n, vector<int>(n, -1));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+        Solution obj;
+        obj.shortestDistance(matrix);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cout << matrix[i][j] << " ";
+            }
+            cout << "\n";
+        }
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
